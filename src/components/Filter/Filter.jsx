d70ce24646c1e33de753a48c9d3fc6c4671abef7
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-20 13:38:08 +0100

    Filter class to function refactoring

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,36 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Filter.module.css';
 
-class Filter extends React.Component {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    onFilterChange: PropTypes.func.isRequired,
-  };
-
-  handleChange = e => {
-    // console.log(e);
+export default function Filter({ value, onFilterChange }) {
+  const handleChange = e => {
     const { value } = e.currentTarget;
-    // console.log(value);
-    this.props.onFilterChange(value);
+    onFilterChange(value);
   };
-
-  render() {
-    const { value } = this.props;
-    return (
-      <div className={css.wrapper}>
-        <label className={css.label}>
-          Find contacts by name
-          <input
-            className={css.input}
-            type="text"
-            name="filter"
-            value={value}
-            onChange={this.handleChange}
-          />
-        </label>
-      </div>
-    );
-  }
+  return (
+    <div className={css.wrapper}>
+      <label className={css.label}>
+        Find contacts by name
+        <input
+          className={css.input}
+          type="text"
+          name="filter"
+          value={value}
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
 }
 
-export default Filter;
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
+};
